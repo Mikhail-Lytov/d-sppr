2e29e7519fcaeea2aa58f0ba111abce115609434
Mihail Lytov <misha.lytov@yadex.ru>
2025-12-17 22:00:10 +0300

    add:  - analyzer

---
+++ d-sppr-analyzer/src/main/java/com/lytov/diplom/dsppranalyzer/DSpprAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.lytov.diplom.dsppranalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DSpprAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DSpprAnalyzerApplication.class, args);
+    }
+
+}
